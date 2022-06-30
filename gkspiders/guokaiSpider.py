4e34f54c858f6f337deb43244de269b62566f8e4
RyanHuang25 <huang.renwu@qq.com>
2022-06-30 17:57:31 +0800

    'up'

--- gkspiders/guokaiSpider.py
+++ gkspiders/guokaiSpider.py
@@ -6,22 +6,29 @@
 @Email: leo.r.huang@microcore.tech
 @Desc: 
 '''
-import requests,pytesseract,execjs,time
+
+import requests,pytesseract,execjs,time,redis,json
 from PIL import Image
 from lxml import etree
+
+pika = redis.Redis(host='47.108.199.19',port=8379,db=4,password='spider666.')
 
 class GuokaiSpider:
 
     def __init__(self):
-        self.userName = '2251001404593'
-        self.pwd = 'Ouchn@2021'
-        self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-        }
-        goto,SunQueryParamsString,validateCode = self.startRequest()
-        oauth2Url = self.loginPost(goto,SunQueryParamsString,validateCode)
-        self.oauth2(oauth2Url)
+        # self.userName = '2251001404593'
+        # self.pwd = 'Ouchn@2021'
+        # self.headers = {
+        #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+        #     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+        # }
+        # goto,SunQueryParamsString,validateCode = self.startRequest()
+        # oauth2Url = self.loginPost(goto,SunQueryParamsString,validateCode)
+        # codeUrl = self.oauth2(oauth2Url)
+        # indexUrl = self.indexCode(codeUrl)
+        # pika.lpush('guokai_cookie',json.dumps(self.cookie))
+        self.cookie = json.loads(pika.lrange('guokai_cookie',0,1)[0])
+        self.learningPC()
 
     def startRequest(self):
         goto, SunQueryParamsString = self.loginPage()
@@ -43,7 +50,7 @@ class GuokaiSpider:
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
         }
-        res = requests.get(indexLoginUrl,headers=headers,allow_redirects=False)
+        res = requests.get(indexLoginUrl,headers=headers,allow_redirects=False,timeout=30)
         self.cookie = requests.utils.dict_from_cookiejar(res.cookies)
         print(self.cookie)
         tree = etree.HTML(res.text)
@@ -62,7 +69,7 @@ class GuokaiSpider:
             "Referer": "https://iam.pt.ouchn.cn/am/UI/Login?realm=/&service=initService&goto=https://iam.pt.ouchn.cn/am/oauth2/authorize?service=initService&response_type=code&client_id=345fcbaf076a4f8a&scope=all&redirect_uri=https%3A%2F%2Fmenhu.pt.ouchn.cn%2Fouchnapp%2Fwap%2Flogin%2Findex&decision=Allow",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
         }
-        res = requests.get(url,headers=headers,cookies=self.cookie)
+        res = requests.get(url,headers=headers,cookies=self.cookie,timeout=30)
         with open('./code.jpg','wb') as fp:
             fp.write(res.content)
         text = pytesseract.image_to_string(Image.open('./code.jpg'),lang='eng')
@@ -87,7 +94,7 @@ class GuokaiSpider:
             "gx_charset": "UTF-8"
         }
         print(f"正在登录：{data}")
-        res = requests.post(url,headers=self.headers,cookies=self.cookie,data=data,allow_redirects=False)
+        res = requests.post(url,headers=self.headers,cookies=self.cookie,data=data,allow_redirects=False,timeout=30)
         cookies = requests.utils.dict_from_cookiejar(res.cookies)
         for key,value in cookies.items():
             self.cookie[key] = value
@@ -118,10 +125,191 @@ class GuokaiSpider:
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
             "Referer": "https://iam.pt.ouchn.cn/am/UI/Login?realm=%2F&service=initService&goto=https%3A%2F%2Fiam.pt.ouchn.cn%2Fam%2Foauth2%2Fauthorize%3Fservice%3DinitService%26response_type%3Dcode%26client_id%3D345fcbaf076a4f8a%26scope%3Dall%26redirect_uri%3Dhttps%253A%252F%252Fmenhu.pt.ouchn.cn%252Fouchnapp%252Fwap%252Flogin%252Findex%26decision%3DAllow"
         }
-        res = requests.get(url,headers=headers,cookies=self.cookie,allow_redirects=False)
+        res = requests.get(url,headers=headers,cookies=self.cookie,allow_redirects=False,timeout=30)
         Location = res.headers['Location']
         return Location
 
+    def indexCode(self,url):
+        '''
+        重定向请求，获取重定向连接和更新cookie
+        :param url:
+        :return:
+        '''
+        res = requests.get(url,headers=self.headers,allow_redirects=False,timeout=30)
+        cookies = requests.utils.dict_from_cookiejar(res.cookies)
+        for key, value in cookies.items():
+            self.cookie[key] = value
+        print(f"cookies已经更新 ===>>> {self.cookie}")
+        Location = res.headers['Location']
+        print(f"重定向url: {Location}")
+        return Location
+
+    def learningPC(self):
+        '''
+        获取到个人课程的所有课程，提取出课程的url
+        :return:
+        '''
+        url = 'https://menhu.pt.ouchn.cn/ouchnapp/wap/course/learning-pc'
+        headers = {
+            "Accept": "application/json, text/plain, */*",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+            "Referer": "https://menhu.pt.ouchn.cn/site/ouchnPc/index"
+        }
+        cookies = {
+            "UUkey": self.cookie['UUkey'],
+            "eai-sess": self.cookie['eai-sess']
+        }
+        res = requests.get(url,headers=headers,cookies=cookies,timeout=30)
+        courseList = res.json()['d']['list']
+        for course in courseList:
+            print(course)
+            courseUrl = course['url']
+            self.modules(courseUrl)
+            break
+
+    def content(self,courseUrl):
+        '''
+        获取课程的章节前的准备信息，获取cookie和refererUrl
+        :param courseUrl:
+        :return:
+        '''
+        # 向课程发起请求，第一次重定向
+        oneHeaders = {
+            "authority": "lms.ouchn.cn",
+            "method": "GET",
+            "path": courseUrl[24:],
+            "scheme": "https",
+            "Referer": "https://menhu.pt.ouchn.cn/",
+            "Host": "lms.ouchn.cn",
+            "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+        }
+        res = requests.get(courseUrl,headers=oneHeaders,allow_redirects=False,timeout=30)
+        oneLocation = res.headers['Location']
+        print(f'重定向url: {oneLocation}')
+        cookies = requests.utils.dict_from_cookiejar(res.cookies)
+        print(f'获取到cookies： {cookies}')
+        # 根据第一次重定向的结果，进行cookie更新并第二次请求发送
+        twoHeaders = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+            "Referer": "https://menhu.pt.ouchn.cn/",
+            "Host": "lms-identity.ouchn.cn"
+        }
+        res = requests.get(oneLocation,headers=twoHeaders,cookies=cookies,timeout=30,allow_redirects=False)
+        twoLocation = res.headers['Location']
+        print(f'重定向url: {twoLocation}')
+        twoCookies = requests.utils.dict_from_cookiejar(res.cookies)
+        for key,value in twoCookies.items():
+            cookies[key] = value
+        print(f'更新cookies： {cookies}')
+        # 根据第二次重定向的结果，进行cookie更新并第三次请求发送
+        threeHeaders = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+            "Host": "lms-identity.ouchn.cn",
+        }
+        res = requests.get(twoLocation, headers=threeHeaders, cookies=cookies, timeout=30, allow_redirects=False)
+        threeLocation = res.headers['Location']
+        print(f'重定向url: {threeLocation}')
+        threeCookies = requests.utils.dict_from_cookiejar(res.cookies)
+        for key, value in threeCookies.items():
+            cookies[key] = value
+        print(f'更新cookies： {cookies}')
+        # 根据第三次重定向的结果，进行cookie更新并第四次请求发送
+        fourHeaders = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+            "Host": "iam.pt.ouchn.cn",
+        }
+        res = requests.get(threeLocation, headers=fourHeaders, cookies=self.cookie, timeout=30, allow_redirects=False)
+        fourLocation = res.headers['Location']
+        print(f'重定向url: {fourLocation}')
+        # 根据第四次请求重定向结果，进行第五次请求发送
+        fiveHeaders = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+            "Host": "lms-identity.ouchn.cn"
+        }
+        res = requests.get(fourLocation,headers=fiveHeaders,cookies=cookies,timeout=30,allow_redirects=False)
+        fiveCookies = requests.utils.dict_from_cookiejar(res.cookies)
+        for key, value in fiveCookies.items():
+            cookies[key] = value
+        print(f'更新cookies： {cookies}')
+        fiveLocation = res.headers['Location']
+        print(f'重定向url: {fiveLocation}')
+        sixHeaders = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+            "Host": "lms.ouchn.cn"
+        }
+        res = requests.get(fiveLocation,headers=sixHeaders,cookies=cookies,timeout=30,allow_redirects=False)
+        sixCookies = requests.utils.dict_from_cookiejar(res.cookies)
+        for key, value in sixCookies.items():
+            cookies[key] = value
+        print(f'更新cookies： {cookies}')
+        sixLocation = res.headers['Location']
+        print(f'重定向url: {sixLocation}')
+        sevenHeaders = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+            "Host": "lms.ouchn.cn"
+        }
+        res = requests.get(sixLocation,headers=sevenHeaders,cookies=cookies,timeout=30,allow_redirects=False)
+        sevenCookies = requests.utils.dict_from_cookiejar(res.cookies)
+        for key, value in sevenCookies.items():
+            cookies[key] = value
+        print(f'更新cookies： {cookies}')
+        sevenLocation = res.headers['Location']
+        print(f'重定向url: {sevenLocation}')
+        eightHeaders = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+            "Host": "lms.ouchn.cn"
+        }
+        res = requests.get(sevenLocation,headers=eightHeaders,cookies=cookies,timeout=30,allow_redirects=False)
+        eightCookies = requests.utils.dict_from_cookiejar(res.cookies)
+        for key, value in eightCookies.items():
+            cookies[key] = value
+        print(f'更新cookies： {cookies}')
+        return sevenLocation,cookies
+
+    def modules(self,courseUrl):
+        courseId = courseUrl[-19:-8]
+        ngUrl,couseCookies = self.content(courseUrl)
+        url = f'https://lms.ouchn.cn/api/courses/{courseId}/modules'
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+            "Referer": ngUrl,
+            "Host": "lms.ouchn.cn"
+        }
+        res = requests.get(url,headers=headers,cookies=couseCookies,allow_redirects=False,timeout=30)
+        modules = res.json()['modules']
+        for module in modules:
+            id = module['id']
+            self.allActivities(courseId,id,couseCookies)
+            break
+
+    def allActivities(self,courseId,id,couseCookies):
+        url = f'https://lms.ouchn.cn/api/course/{courseId}/all-activities?module_ids=[{id}]&activity_types=learning_activities,exams,classrooms,live_records,rollcalls&no-loading-animation=true'
+        headers = {
+            "authority": "lms.ouchn.cn",
+            "method": "GET",
+            "path": f"/api/course/{courseId}/all-activities?module_ids=[{id}]&activity_types=learning_activities,exams,classrooms,live_records,rollcalls&no-loading-animation=true",
+            "scheme": "https"
+        }
+        res = requests.get(url,headers=headers,cookies=couseCookies,timeout=30)
+        # 学习
+        learning_activities = res.json()['learning_activities']
+        for learning_activity in learning_activities:
+            if learning_activity['type'] == "page":
+                # 学习文本
+                pass
+        # 测试
+
+    def notes(self):
+        '''
+        请求观看的视频，并且模拟观看
+        :return:
+        '''
+        url = 'https://lms.ouchn.cn/api/notes'
+        params = {
+            "course_id": "",
+            "activity_id": "",
+            "target_id": ""
+        }
 
 if __name__ == '__main__':
     GuokaiSpider()
